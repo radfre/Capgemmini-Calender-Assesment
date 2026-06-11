1f54adb2410d8bee35c4c5766f27eeabf66429d6
radfre <schatzalex97@gmail.com>
2026-06-11 00:51:00 -0500

    udated time slot check

--- Capgemmini Calender/src/Day.java
+++ Capgemmini Calender/src/Day.java
@@ -1,32 +1,49 @@
 import java.util.ArrayList;
 
 public class Day {
-    private ArrayList<Events> events;
+    private ArrayList<Event> events;
     
     public Day(){
-
+        events = new ArrayList<Event>();
+        events.add(new Event(0,0, "Start of day"));
+        events.add(new Event(1440,0, "End of day"));
     }
     
-    public ArrayList<Events> listAllEvents(){
+    public ArrayList<Event> listAllEvents(){
         return events;
     }
 
-    public ArrayList<Events> listAllRemainingEvents(int[] startTime){
-        
-        // filter events based on the provided start time
-        return events;
-
+    public ArrayList<Event> listAllRemainingEvents(int startTime){
+        ArrayList<Event> remainingEvents = new ArrayList<Event>();
+        for (Event event : events){
+            if (event.getStartTime() >= startTime){
+                remainingEvents.add(event); 
+            }
+        }
+        return remainingEvents;
     }
 
-    public void addEvent(Events event){
+    public void addEvent(Event event){
+        for(Event existingEvent : events){
+            if (existingEvent.overlapsWith(event)){
+                return; // do not add the event if it overlaps with an existing event
+            }
+        }
         events.add(event);
     }
 
-    public void removeEvent(Events event){
+    public void removeEvent(Event event){
         events.remove(event);
     }
 
-    public void nextAvaibleTimeSlot(int[] startTime, int durationMinutes){
-        // logic to find the next available time slot based on the existing events and the provided duration
+    public Event nextAvailableTimeSlot(int startTime, int duration){
+        events.sort((e1, e2) -> Integer.compare(e1.getStartTime(), e2.getStartTime())); // sort events by start time
+        for (int i = 0; i < events.size()-1; i++){
+            if(events.get(i).getEndTime() >= startTime && events.get(i+1).getStartTime() >= startTime + duration){
+                return new Event(events.get(i).getEndTime(), duration); // return the next available time slot as an event
+            }
+
+        }
+        return new Event(0, 0); // return a default event if no available time slot is found
     }
 }
