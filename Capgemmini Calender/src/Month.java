a5735434da94fb8ab9add72016b369b4f221bd7c
radfre <schatzalex97@gmail.com>
2026-06-11 00:53:00 -0500

    added functionality

--- Capgemmini Calender/src/Month.java
+++ Capgemmini Calender/src/Month.java
@@ -1,7 +1,17 @@
 public class Month {
-    arrayList<Day> days;
+    private Day[] days;
 
     public Month(){
-        
+        days = new Day[30]; 
+        for (Day day : days) {
+            day = new Day(); 
+        }
+    }
+
+    public Day getDay(int dayNumber){
+        if (dayNumber >= 0 && dayNumber <= days.length-1) {
+            return days[dayNumber]; // return the requested day (adjusting for 0-based index)
+        }
+        return null; // return null if the day number is out of range
     }
 }
